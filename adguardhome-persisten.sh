8fede2bc0a1cd4fd1f91f5d68a4ccc55c4838768
test-base <67887101+test-base@users.noreply.github.com>
2022-06-03 16:57:44 +0700

    Update adguardhome-persisten.sh
    v1.0.6

--- adguardhome-persisten.sh
+++ adguardhome-persisten.sh
@@ -9,4 +9,4 @@ mv /var/adguardhome/* /etc/AdGuardHome 1 >/dev/null 2>&1
 sed -i 's_/var/adguardhome_/etc/AdGuardHome_g' \
   /etc/config/adguardhome >/dev/null 2>&1
 service adguardhome stop >/dev/null 2>&1; sleep 2
-printf "\nSelesai!\n\n"
+printf "Selesai!\n\n"
