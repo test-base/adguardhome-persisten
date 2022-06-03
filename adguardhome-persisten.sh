a37aaa5c9e5ff6541ea439fc4ae40a2e413aec5e
test-base <67887101+test-base@users.noreply.github.com>
2022-06-03 16:49:34 +0700

    Update adguardhome-persisten.sh
    v1.0.4

--- adguardhome-persisten.sh
+++ adguardhome-persisten.sh
@@ -3,10 +3,10 @@
 # Description:
 # Dibuat untuk memperbaiki filter adguard yg selalu hilang setelah reboot
 
-echo -n "\n\nMemperbaiki AdGuardHome, mohon tunggu...\n"
+printf "\n\nMemperbaiki AdGuardHome, mohon tunggu...\n"
 service adguardhome stop >/dev/null 2>&1; sleep 2
 mv /var/adguardhome/* /etc/AdGuardHome 1 >/dev/null 2>&1
 sed -i 's_/var/adguardhome_/etc/AdGuardHome_g' \
   /etc/config/adguardhome >/dev/null 2>&1
 service adguardhome stop >/dev/null 2>&1; sleep 2
-echo -n "\nSelesai!\n\n"
+printf -n "\nSelesai!\n\n"
